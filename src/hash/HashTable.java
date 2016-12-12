1395a512c1df84072803ce903252f07adf0471ef
Conkilla <mahonecon@gmail.com>
2016-12-12 10:08:38 -0500

    HashTable 2.0

--- src/hash/HashTable.java
+++ src/hash/HashTable.java
@@ -4,21 +4,28 @@ import list.*;
 /**
  * A table which allows for quick access to data. Duplicates are permitted.
  * E should define equals(Object) and hashCode()
+ * 2.0 adjusts table size for efficiency
+ * 
  * @author CJ Mahoney
+ * 
+ * @version 2.0
  *
  */
 public class HashTable<E> {
 
-	List<List<E>> lists = new ArrayList<List<E>>();
+	List<List<E>> lists = new ArrayList<List<E>>(); //Creates an ArrayList of Lists of type E for the HashTable.
 	int keyCount = 0; //size
+	static final int MAX = 10; //2.0: MAX constant for determining whether or not to rebuild.
+	int maxList = 0; //2.0: Length of the longest Linked List.
 	
 	public HashTable() { //default capacity is 10
 		this(10);
 	}
+	
 	/** @param capacity is size of ArrayList*/
 	public HashTable(int capacity) {
 		for(int i = 0; i < capacity; i++)
-			lists.add(new LinkedList<E>());
+			lists.add(new LinkedList<E>()); //Creates capacity worth of empty Linked Lists.
 	}
 	
 	/**return a valid index to lists */
@@ -35,11 +42,17 @@ public class HashTable<E> {
 		return lists.get(code).contains(obj);
 	}
 	
-	/** Add the given value to this HashTable*/
-	public void put(E value) {
-		int code = getCode(value);
-		lists.get(code).add(value);
+	/** Add the given value to this HashTable
+	 * 2.0: Rebuild the table if longest list > MAX */
+	public void put(E key) {
+		int code = getCode(key);
+		List<E> list = lists.get(code);
+		list.add(key);
 		keyCount++;
+		if(list.size() > maxList) { //2.0: If list is the largest list
+			maxList = list.size(); //2.0: Set maxList to list.size()
+			rehash(); //2.0: rebuild hash table.
+		}
 	}
 	
 	/**@return the given value from this HashTable, or null
@@ -64,10 +77,12 @@ public class HashTable<E> {
 		return false;
 	}
 	
+	/** @return the number of keys in the hashtable*/
 	public int size() {
 		return keyCount;
 	}
 	
+	/** @return true if there are no keys in the hashtable */
 	public boolean isEmpty() {
 		if(keyCount != 0) {
 			return false;
@@ -75,12 +90,36 @@ public class HashTable<E> {
 		return true;
 	}
 	
+	/** Clears the hashtable */
 	public void clear() {
 		lists.clear();
 		keyCount = 0;
 	}
 	
+	/** @return an Iterator to iterate through the HashTable */
 	public Iterator<E> iterator() {
 		return new TableIterator<E>(this);
+	}
+	
+	/** 2.0: Rebuilds the hash table if necessary.  When rebuilt, doubles the amount of Linked Lists
+	 * contained in the HashTable, to make each individual Linked List smaller. */
+	private void rehash() {
+		if(maxList <= MAX) //To rebuild or not to rebuild, that is the question.
+			return;
+		List<List<E>> table = new ArrayList<List<E>>();
+		for(int i = 0; i < lists.size() * 2; i++) { //Create a table populate with twice as many Linked Lists.
+			table.add(new LinkedList<E>());
+		}
+		Iterator<E> itty = iterator();
+		while(itty.hasNext()) {
+			E value = itty.next();
+			int code = Math.abs(value.hashCode());
+			code = code % table.size();
+			List<E> list = table.get(code);
+			list.add(value);
+			if(list.size() > maxList)
+				maxList = list.size();
+		}
+		lists = table;
 	}
 }
